c43eb8a585b33a95975d571aedce4c8836be5f1a
ayastaga <113945551+ayastaga@users.noreply.github.com>
2025-09-04 10:04:06 -0400

    Update page.tsx
    removed comments for other types of components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,43 +15,7 @@ import ImageUploadAnalysisCard from "./components/image-upload-analysis";
 export default function IndexPage() {
   return (
     <Shell className="max-w-2xl">
-
       <ImageUploadAnalysisCard/>
-
-      {/* Basic card
-      <Card>
-        <CardHeader>
-          <CardTitle>Basic</CardTitle>
-          <CardDescription>Basic controlled file upload.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <FileUploadDemo />
-        </CardContent>
-      </Card>
-
-      {/* React hook form
-      <Card>
-        <CardHeader>
-          <CardTitle>React hook form</CardTitle>
-          <CardDescription>
-            File upload integration with React Hook Form.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <FileUploadFormDemo />
-        </CardContent>
-      </Card>
-    
-      <Card>
-        <CardHeader>
-          <CardTitle>uploadthing</CardTitle>
-          <CardDescription>File upload using uploadthing.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <FileUploadUploadThingDemo />
-        </CardContent>
-      </Card>
-      */}
     </Shell>
   );
 }
