dfdba9d1985150f219433cefd6f39450c30f98f1
ayastaga <113945551+ayastaga@users.noreply.github.com>
2025-09-03 22:29:04 -0400

    Create site.ts
    need to change & edit this file in the near future since it contains useless code

---
+++ src/config/site.ts
@@ -0,0 +1,16 @@
+import { env } from "@/env";
+
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Calorie Counter",
+  description: "Upload any image and count the number of calories present!",
+  url:
+    env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://uploader.sadmn.com",
+  links: {
+    github: "https://github.com/ayastaga",
+    docs: "https://diceui.com/docs/components/",
+  },
+};
