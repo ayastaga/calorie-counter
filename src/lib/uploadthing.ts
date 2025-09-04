575d3839aef84943a5756e52398507e75d95358a
ayastaga <113945551+ayastaga@users.noreply.github.com>
2025-09-04 09:31:48 -0400

    Update uploadthing.ts

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,17 +1,10 @@
 // /src/lib/uploadthing.ts
 import {
-  generateUploadButton,
-  generateUploadDropzone,
   generateReactHelpers,
 } from "@uploadthing/react/hooks";
-import { generateComponents } from "@uploadthing/react";
 
-import type { OurFileRouter } from "~/app/api/uploadthing/core";
+import type { OurFileRouter } from "../app/api/uploadthing/core";
 
-export const UploadButton = generateUploadButton<OurFileRouter>();
-export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
-
-export const { uploadFiles } = generateReactHelpers<OurFileRouter>();
-
-export const { UploadButton: UTUploadButton, UploadDropzone: UTUploadDropzone } =
-  generateComponents<OurFileRouter>();
+export const { uploadFiles } = generateReactHelpers<OurFileRouter>({
+  url: "/api/uploadthing", // Explicitly set the URL
+});
