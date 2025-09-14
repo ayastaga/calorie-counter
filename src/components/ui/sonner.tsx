571f63588bb39f6e2c0c0fa3838d04554d997202
ayastaga <ayavasu@gmail.com>
2025-09-14 01:27:02 -0400

    changed the color of the toaster (FINALLY!!!)

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -4,18 +4,22 @@ import { useTheme } from "next-themes";
 import { Toaster as Sonner, type ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
+  const { theme = "neutral" } = useTheme();
 
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
-      className="toaster group"
+      className="toaster group bg"
+      toastOptions={{
+        classNames: {
+          description: "text-muted-foreground", // or your custom class
+        },
+      }}
       style={
         {
           "--normal-bg": "var(--popover)",
           "--normal-text": "var(--popover-foreground)",
           "--normal-border": "var(--border)",
-          "--toast-description-color": "#000000",
         } as React.CSSProperties
       }
       {...props}
